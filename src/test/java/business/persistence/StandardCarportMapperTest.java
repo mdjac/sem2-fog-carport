5de5dc0a5e603e865ab60c514e3755ce98240378
Mathias <madr1402@hotmail.com>
2021-05-21 10:53:26 +0200

    standard carport test

--- src/test/java/business/persistence/StandardCarportMapperTest.java
+++ src/test/java/business/persistence/StandardCarportMapperTest.java
@@ -8,6 +8,7 @@ import business.exceptions.UserException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import web.StaticValues;
 
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,6 +26,7 @@ class StandardCarportMapperTest {
     private static Database database;
     private static MaterialMapper materialMapper;
     private static StandardCarportMapper standardCarportMapper;
+    private static OrderMapper orderMapper;
 
     @BeforeAll
     public static void setUpClass() {
@@ -32,7 +34,7 @@ class StandardCarportMapperTest {
             database = new Database(USER, PASSWORD, URL);
             materialMapper = new MaterialMapper(database);
             standardCarportMapper = new StandardCarportMapper(database);
-
+            new StaticValues().setGlobalValues(database);
         } catch (ClassNotFoundException e) {   // kan ikke finde driveren i database klassen
             fail("Database connection failed. Missing jdbc driver");
         }
@@ -59,13 +61,11 @@ class StandardCarportMapperTest {
     @Test
     void insertStandardCarport() throws UserException {
 
-        Material materialId = new Material("Carport Beklædning","Trykimp. Brædt",1,1,1,1,10);
-        Material roof = new Material("Fladt tag materiale","Plastmo Ecolite blåtonet",3,2,31,1,250);
-
-        Carport carport = new Carport(materialId,300,250,600,RoofType.Fladt_Tag,roof);
-        carport.setId(2);
+        Carport carport = new Carport(Carport.findCarportMaterialFromId(1),300,250,600,RoofType.Fladt_Tag,Carport.findRoofMaterialFromId(3,RoofType.Fladt_Tag));
+        carport.setId(4);
         standardCarportMapper.insertStandardCarport(carport);
+        System.out.println(carport);
 
-        assertEquals(2,carport.getId());
+        assertEquals(4,carport.getId());
     }
 }
